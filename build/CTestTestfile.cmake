ccd9c9224bb64c6d4befad902114049121bd65dc
joehpaulus <143455279+joehpaulus@users.noreply.github.com>
2025-02-07 16:03:30 -0700

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jovyan/CSCI 2270/final-project-joe
+# Build directory: /home/jovyan/CSCI 2270/final-project-joe/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
